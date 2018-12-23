1c3fed29fe64a2c3dec54db19220fca3fd7add7a
Syeda Tasmiah Asad <44003304+TASMIAHASAD@users.noreply.github.com>
2018-12-23 00:07:58 -0800

    Add files via upload
    vector selection sort main added

---
+++ vector selection sort main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print (vector<int> & vec) {
+    for (int i =0 ; i < vec.size(); ++i) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+int temp;
+vector<int> sort;
+sort.push_back(14);
+sort.push_back(33);
+sort.push_back(27);
+sort.push_back( 10);
+sort.push_back(35);
+sort.push_back(19);
+sort.push_back(48);
+sort.push_back(44);
+
+print(sort);
+
+int vecsize = sort.size();
+for (int j = 0; j < vecsize - 1; ++j) {
+
+    int min = j;
+    for (int i = j+1; i < vecsize; ++i) {
+        if (sort.at(min) > sort.at(i)) {
+            min = i;
+        }
+
+    }
+    if (min != j)
+        swap(sort.at(j), sort.at(min));
+}
+
+print(sort);
+return 0;
+}
